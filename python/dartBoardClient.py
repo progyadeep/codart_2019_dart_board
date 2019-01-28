aa9d7fcccc3827a4125d64aae48e67448d8fcea3
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-01-28 16:51:25 +0000

    Add files via upload

--- python/dartBoardClient.py
+++ python/dartBoardClient.py
@@ -1,13 +1,17 @@
 import serial
 import requests
 
+teams = open("teams.txt").read().split("\n")
+teams = [x.split(".")[1] for x in teams]
+
 ser = serial.Serial("COM6", 9600)
 post = 3
 print("Running POST check...")
 l = 0
 
 while True:
-	x = str(ser.read())[5]
+	t = int(input("TEAM: ").strip())
+	x = int(str(ser.read())[5])
 	if post > 0:
 		post = post - 1
 		if post == 0:
@@ -16,5 +20,5 @@ while True:
 		if l == 1:
 			l = 0
 		else:
-			print(str(requests.get("http://127.0.0.1/codart/?param="+x).content)[2:].replace("'", ""))
-			l = int(x)
+			print(str(requests.post("http://127.0.0.1/codart", data={val:str(x-1).content[2:].replace("'", ""), team:teams[t-1]})
+			l = x
