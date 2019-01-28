97caf9e69de15419ef41b6f12159c4887d6e7a74
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-01-28 22:48:35 +0530

    Update dartBoardClient.py

--- python/dartBoardClient.py
+++ python/dartBoardClient.py
@@ -7,18 +7,19 @@ teams = [x.split(".")[1] for x in teams]
 ser = serial.Serial("COM6", 9600)
 post = 3
 print("Running POST check...")
-l = 0
+
+while post > 0:
+	s = ser.read()
+	post = post - 1
+
+print("READY!\n")
+l = "0"
 
 while True:
 	t = int(input("TEAM: ").strip())
-	x = int(str(ser.read())[5])
-	if post > 0:
-		post = post - 1
-		if post == 0:
-			print("READY!\n");
+	x = str(ser.read())[5]
+	if l == "1":
+		l = "0"
 	else:
-		if l == 1:
-			l = 0
-		else:
-			print(str(requests.post("http://127.0.0.1/codart", data={val:str(x-1).content[2:].replace("'", ""), team:teams[t-1]})))
-			l = x
+		print(str(requests.post("http://127.0.0.1/codart", data={'val':x, 'team':teams[t-1]})))
+		l = x
