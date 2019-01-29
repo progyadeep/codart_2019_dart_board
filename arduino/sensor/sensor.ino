48d1499e2fd220f88be7510502629501a06ea014
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-01-29 16:14:09 +0530

    Update sensor.ino

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -1,29 +1,22 @@
-int chart[][2] = {{7, 0}, {6, 0}, {5, 0}};
+int chart[][2] = {{7,0},{6,0},{5,0}};
+int v,i;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 0; i <= 2; i++) {
+  for(i=0; i<=2; i++)
     pinMode(chart[i][0], INPUT);
-    //pinMode(chart[i][1], OUTPUT);
-  }
+  
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 }
 
 void loop() {
-  int v, i = -1;
-  if ((v = digitalRead(chart[0][0])) != chart[0][1])
-  {
-    chart[0][1] = v;
-    Serial.println(0);
+  for(i=0; i<=2; i++){
+   if((v=digitalRead(chart[i][0])) != chart[i][1]){
+     chart[i][1] = v;
+     Serial.println(i);
+     delay(100);
+     break;
+   }
   }
-  else if ((v = digitalRead(chart[1][0])) != chart[1][1]) {
-    chart[1][1] = v;
-    Serial.println(1);
-  }
-  else if ((v = digitalRead(chart[2][0])) != chart[2][1]) {
-    chart[2][1] = v;
-    Serial.println(2);
-  }
-  delay(200);
 }
