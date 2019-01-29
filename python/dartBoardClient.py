442c31a23820c607ffe8d3ec2b415f50d7a82e79
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-01-29 23:43:50 +0530

    Update dartBoardClient.py

--- python/dartBoardClient.py
+++ python/dartBoardClient.py
@@ -13,13 +13,17 @@ while post > 0:
 	post = post - 1
 
 print("READY!\n")
-l = "0"
+l = "#"
 
+print("\nHIT: ", end='')
 while True:
-	t = int(input("TEAM: ").strip())
 	x = str(ser.read())[5]
-	if l == "1":
-		l = "0"
+	if l == x:
+		l = "#"
 	else:
-		print(str(requests.post("http://127.0.0.1/codart", data={'val':x, 'team':teams[t-1]})))
+		print("OK ("+str(x)+")")
+		t = int(input("TEAM: ").strip())
+		print(str(requests.post("http://127.0.0.1/", data={'dart':x, 'team':teams[t-1], 'token':'A70811DBX'}).text))
+		#print(x)
 		l = x
+		print("\nHIT: ", end='')
