e74c3c53d08d0cd0a632fe996b6b7dbbe24427b2
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-02-19 03:54:19 +0530

    Final commit

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -1,22 +1,27 @@
-int chart[][2] = {{7,0},{6,0},{5,0}};
-int v,i;
+int chart[][3] = {{7,0,13},{6,0,12},{4,0,11}};
+int ops[] = {0,0,0};
+int i;
 
 void setup() {
   Serial.begin(9600);
-  for(i=0; i<=2; i++)
+  for(i=0; i<=2; i++){
     pinMode(chart[i][0], INPUT);
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+    pinMode(chart[i][2], OUTPUT);
+  }
 }
 
 void loop() {
   for(i=0; i<=2; i++){
-   if((v=digitalRead(chart[i][0])) != chart[i][1]){
-     chart[i][1] = v;
-     Serial.println(i);
-     delay(100);
-     break;
-   }
+      ops[i] = digitalRead(chart[i][0]);
+  }
+  for(i=2; i>=0; i--){
+    if(chart[i][1] != ops[i]){
+      chart[i][1] = ops[i];
+      Serial.write(i);
+      digitalWrite(chart[i][2], HIGH);
+      delay(200);
+      digitalWrite(chart[i][2], LOW);
+      break;
+    }
   }
 }
