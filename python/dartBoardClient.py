ba981adb1fdc403386ecd35eda51108e00537189
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-02-19 03:56:00 +0530

    Final commit

--- python/dartBoardClient.py
+++ python/dartBoardClient.py
@@ -1,8 +1,6 @@
 import serial
 import requests
 
-teams = open("teams.txt").read().split("\n")
-teams = [x.split(".")[1] for x in teams]
 diff = ['medium', 'hard', 'easy']
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWRtaW4iLCJpYXQiOjE1NDk2NDM4MTl9.x7tS75lXAFcNXCb29ToX-Cyt8d1F9aSt1Ba_ILTedRc"
 
@@ -22,8 +20,6 @@ while True:
 		l = "#"
 	else:
 		print("OK ("+str(x)+")")
-		#t = int(input("TEAM: ").strip())
 		print(str(requests.post("http://192.168.1.105:5000/assign", data={'level':diff[int(x)], 'token':token}).text))
-		#print(x)
 		l = x
 	ser.close()
