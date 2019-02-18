0da85230fe800f1b09805fcc95cd01082f071505
Progyadeep Moulik <progyadeep@rediffmail.com>
2019-02-19 03:54:54 +0530

    Final commit

--- python/dartBoardClient.py
+++ python/dartBoardClient.py
@@ -3,26 +3,27 @@ import requests
 
 teams = open("teams.txt").read().split("\n")
 teams = [x.split(".")[1] for x in teams]
+diff = ['medium', 'hard', 'easy']
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWRtaW4iLCJpYXQiOjE1NDk2NDM4MTl9.x7tS75lXAFcNXCb29ToX-Cyt8d1F9aSt1Ba_ILTedRc"
 
-ser = serial.Serial("COM6", 9600)
-post = 3
-print("Running POST check...")
-
-while post > 0:
-	s = ser.read()
-	post = post - 1
-
-print("READY!\n")
-l = "#"
-
-print("\nHIT: ", end='')
 while True:
+	ser = serial.Serial("COM6", 9600)
+	post = 3
+	print("\n\nRunning POST check...")
+
+	while post > 0:
+		s = ser.read()
+		post = post - 1
+
+	print("READY!\nHIT: ", end='')
+	l = "#"
 	x = str(ser.read())[5]
 	if l == x:
 		l = "#"
 	else:
 		print("OK ("+str(x)+")")
-		t = int(input("TEAM: ").strip())
-		print(str(requests.post("http://127.0.0.1/", data={'dart':x, 'team':teams[t-1], 'token':'A70811DBX'}).text))
+		#t = int(input("TEAM: ").strip())
+		print(str(requests.post("http://192.168.1.105:5000/assign", data={'level':diff[int(x)], 'token':token}).text))
+		#print(x)
 		l = x
-		print("\nHIT: ", end='')
+	ser.close()
